4d7e07a3eadee695b6b53ad604833abfa1f7fb09
PratikPatel3900 <PratikPatel3900>
2020-12-08 21:22:43 -0600

    Submission for Module 10- Pratik Patel

---
+++ Module 10/BlazorApp/Pages/TodoItem.cs
@@ -0,0 +1,10 @@
+/* by addding 
+the root of the project (the TodoList folder) to hold a class 
+that represents a todo item. */
+
+/* root of todo project */
+public class TodoItem
+{
+    public string Title { get; set;}
+    public bool IsDone { get; set;}
+}
